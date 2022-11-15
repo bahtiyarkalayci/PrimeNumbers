80bf519d6cb8599193579e549d4e272c5704ac4b
bahtiyarkalayci <49812375+bahtiyarkalayci@users.noreply.github.com>
2022-11-15 14:12:30 +0300

    https://www.patika.dev/tr

--- src/PrimeNumbers.java
+++ src/PrimeNumbers.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class PrimeNumbers {
+import java.util.Scanner;
+
+public class PrimeNumbers {
+    public static void main(String[] args) {
+
+        for (int i=2;i<100;i++){
+
+            if (i==2||i==3){
+                System.out.println("prime number="+i);
+            }
+
+              if (((i%2!=0)&&(i%3!=0))&&((i%5!=0)&&(i%7!=0))){
+                  System.out.println("prime number="+i);
+              }
+
+
+        }
+    }
 }
